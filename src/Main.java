e03f7da8c205a441df09953f07a56c14c9c3226a
Serdar Arici <serdararici3@gmail.com>
2025-06-12 09:04:31 +0300

    project created and pseudo code prepared

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String args[]) {
+
+        // JAVA SLOT MACHINE
+
+        // DECLARE VARIABLES
+        // DISPLAY WELCOME MESSAGE
+        // PLAY IF BALANCE > 0
+        // ENTER BET AMOUNT
+        //      VERIFY IF BET > BALANCE
+        //      VERIFY IF BET > 0
+        //      SUBTRACT BET FROM BALANCE
+        // SPIN ROW
+        // PRINT ROW
+        // GET PAYOUT
+        // ASK TO PLAY AGAIN
+        // DISPLAY EXIT MESSAGE
+    }
+}
