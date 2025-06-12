5e5898963e56d769d5ce8ded3a5e3842cb067f72
Serdar Arici <serdararici3@gmail.com>
2025-06-12 09:26:15 +0300

    initialize Java Slot Machine game structure with balance, bet input, and validation logic

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,57 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         // JAVA SLOT MACHINE
 
         // DECLARE VARIABLES
+
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
         // DISPLAY WELCOME MESSAGE
+
+        System.out.println("******************************");
+        System.out.println("**  Welcome to Java Slots  **");
+        System.out.println("** Symbols: 🍒 🍉 🍋 🔔 ⭐ **");
+        System.out.println("******************************");
+
         // PLAY IF BALANCE > 0
-        // ENTER BET AMOUNT
-        //      VERIFY IF BET > BALANCE
-        //      VERIFY IF BET > 0
-        //      SUBTRACT BET FROM BALANCE
+
+        while (balance > 0) {
+            System.out.println("Current balance: $" + balance);
+            System.out.println("Place your bet amount: ");
+
+            // ENTER BET AMOUNT
+            bet = scanner.nextInt();
+
+            //      VERIFY IF BET > BALANCE
+            if(bet > balance) {
+                System.out.println("INSUFFICIENT FUNDS");
+                continue;
+
+            //      VERIFY IF BET > 0
+            } else if(bet <= 0) {
+                System.out.println("Bet must be greater than 0");
+                continue;
+
+            //      SUBTRACT BET FROM BALANCE
+            } else {
+                balance -= bet;
+            }
+        }
+
         // SPIN ROW
+
         // PRINT ROW
         // GET PAYOUT
         // ASK TO PLAY AGAIN
         // DISPLAY EXIT MESSAGE
+
+        scanner.close();
     }
 }
